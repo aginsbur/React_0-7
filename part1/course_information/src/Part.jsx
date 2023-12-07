61b47371f8b97f031b9b8fa47ada251735a330ac
aginsbur <ataraeg613@gmail.com>
2023-12-07 10:41:38 +0200

    testing

--- part1/course_information/src/Part.jsx
+++ part1/course_information/src/Part.jsx
@@ -1,11 +1,11 @@
-const Part = (props)=>{
-    const part=props.part;
-    const exercise=props.exercise;
-    return (
-      <div>
-       <p>  {part} {exercise}</p>
-      </div>
-    )
-  }
+const Part = ({ part, exercise }) => {
+  return (
+    <div>
+      <p>
+        {part} {exercise}
+      </p>
+    </div>
+  );
+};
 
-  export default Part;
+export default Part;
