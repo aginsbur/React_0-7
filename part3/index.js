2b916f789ce00d753c979ecdf551e06cc698d0ac
aginsbur <ataraeg613@gmail.com>
2023-12-11 11:12:38 +0200

    dist

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.json())
+app.use(express.static('dist'))
 
 const logger = require('morgan')
 logger.token('body',(req)=>JSON.stringify(req.body))
@@ -107,7 +108,8 @@ app.get('/api/persons/:id', (request, response) => {
     response.json(person)
   })
 
-  const PORT = process.env.PORT || 3001
+  const PORT = process.env.PORT || 3001;
+
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
